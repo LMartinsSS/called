08f7c461ed9bbb7eaaa91344f2972c3f81719889
LMartinsSS <martins.henriqueluis.santos@gmail.com>
2023-08-31 20:24:41 -0300

    feat: add conditions in sqs service

--- src/aws/sqs.js
+++ src/aws/sqs.js
@@ -1,24 +1,29 @@
 const { sqs } = require("../config/aws");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const queueUrl = process.env.QUEUEURL
 
 class Queue {
-    listQueue() {
+    listQueue(req, res) {
         sqs.listQueues({}, function (err, data) {
             if (err) {
-                console.log("Error", err);
-            } else {
-                console.log("Success", data.QueueUrls);
-            }
+                res.send(err).status(404)
+            } else if (data.QueueUrls)
+                res.send({ queueUrls: data.QueueUrls }).status(200)
         });
     }
 
-    receiveMessage() {
+    receiveMessage(req, res) {
         sqs.receiveMessage({
-            QueueUrl: "https://sqs.us-east-1.amazonaws.com/182037990636/called"
+            QueueUrl: queueUrl
         }, (err, data) => {
             if (err)
-                console.log("Error", err);
-            else
-                console.log(data)
+                res.send(err).status(404)
+            else if (data.Messages) {
+                const message = data.Messages[0].Body
+                res.send({ content: message }).status(200)
+            }
         })
     }
 }
