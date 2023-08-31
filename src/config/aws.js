5e56341478455773aa4a50dc26a154f02f532b0d
LMartinsSS <martins.henriqueluis.santos@gmail.com>
2023-08-31 20:22:40 -0300

    feat: add variables in dotenv

--- src/config/aws.js
+++ src/config/aws.js
@@ -1,11 +1,14 @@
 const { SQS } = require("@aws-sdk/client-sqs");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const region = process.env.REGION
 const apiVersion = process.env.APIVERSION
 
 var sqs = new SQS({
-    region,
-    apiVersion
+    region: region,
+    apiVersion: apiVersion
 });
 
 module.exports = { sqs }
